529bb1cacb85d5aab2173ef39e5aba358bc564e4
louispawaon <lpawaon@gmail.com>
2023-05-14 11:10:41 +0800

    Create Custom Scalar Types for Website and Email fields

--- books/schema.py
+++ books/schema.py
@@ -12,7 +12,7 @@ Custom Scalar Types
 # Website Custom Scalar Type
 class Website(graphene.Scalar):
     website_pattern = re.compile(
-        r"^((http|https)://)[-a-zA-Z0-9@:%._\\+~#?&//=]{2,256}\\.[a-z]{2,6}\\b([-a-zA-Z0-9@:%._\\+~#?&//=]*)$"
+        r"^https?:\/\/(?:www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b(?:[-a-zA-Z0-9()@:%_\+.~#?&\/=]*)$"
     )
 
     @staticmethod
